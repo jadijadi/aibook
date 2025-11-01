94c4b9db323beb5218ec2e768a3b1967fb0fd5d5
Jadi <jadijadi@gmail.com>
2025-11-01 14:52:22 -0700

    adding stats in C

---
+++ stats.c
@@ -0,0 +1,14 @@
+// write a small routine to calculate the stats of a file
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <stdbool.h>
+#include <time.h>
+#include <math.h>
+#include <stdarg.h>
+#include <stddef.h>
